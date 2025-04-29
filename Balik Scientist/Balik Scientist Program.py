da37383bc557f18658bbd52e6eada98ec3c07573
Benjamin Tumbaga <bnjmntumbaga@gmail.com>
2025-04-29 14:51:11 +0800

    update input

--- Balik Scientist/Balik Scientist Program.py
+++ Balik Scientist/Balik Scientist Program.py
@@ -21,10 +21,10 @@ wait.until(EC.element_to_be_clickable((By.ID, "Medium-term_section"))).click()
 repatriate_option = wait.until(EC.presence_of_element_located((By.ID, "repatriate_1")))
 driver.execute_script("arguments[0].click();", repatriate_option)
 
-wait.until(EC.presence_of_element_located((By.NAME, "last_name"))).send_keys("Test")
-driver.find_element(By.NAME, "first_name").send_keys("test 1")
+wait.until(EC.presence_of_element_located((By.NAME, "last_name"))).send_keys("Benjamin")
+driver.find_element(By.NAME, "first_name").send_keys("Tumbaga")
 driver.find_element(By.NAME, "name_extension").send_keys("Jr.")
-driver.find_element(By.NAME, "middle_name").send_keys("test 1")
+driver.find_element(By.NAME, "middle_name").send_keys("Sabado")
 driver.find_element(By.NAME, "maiden_name").send_keys("N/A")
 
 male_option = wait.until(EC.presence_of_element_located((By.ID, "sex_M")))
@@ -33,7 +33,7 @@ driver.execute_script("arguments[0].click();", male_option)
 civil_status_option = wait.until(EC.presence_of_element_located((By.ID, "civil_status_S")))
 driver.execute_script("arguments[0].click();", civil_status_option)
 
-driver.find_element(By.NAME, "email_address").send_keys("test@gmail.com")
+driver.find_element(By.NAME, "email_address").send_keys("QA.InternKSU.LODIxR@gmail.com")
 
 
 old_url = driver.current_url
